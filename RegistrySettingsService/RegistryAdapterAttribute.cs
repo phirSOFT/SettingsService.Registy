d71cdb13456616b5bfa721dc6e6ea2fd86fd2225
Philemon Eichin <phileicht@gmx.de>
2018-08-17 11:52:08 +0200

    Add RegistryAdapterAttribute

---
+++ RegistrySettingsService/RegistryAdapterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace phirSOFT.SettingsService.Registry
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Marks an class as an <see cref="T:phirSOFT.SettingsService.Registry.IRegistryAdapter" /> for a given type.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+    public sealed class RegistryAdapterAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets the type supported by this adapter.
+        /// </summary>
+        public Type SupportedType { get; }
+
+        /// <summary>
+        ///  Marks an class as an <see cref="T:phirSOFT.SettingsService.Registry.IRegistryAdapter" /> for a given type.
+        /// </summary>
+        /// <param name="supportedType">The type supported by this adapter.</param>
+        public RegistryAdapterAttribute(Type supportedType)
+        {
+            SupportedType = supportedType;
+        }
+    }
+}
